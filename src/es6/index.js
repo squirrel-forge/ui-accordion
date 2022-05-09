129278e174312a974865b6179f0c260b69a8d3bd
dasiux <me@siux.info>
2022-05-09 23:00:16 +0200

    Create index.js

---
+++ src/es6/index.js
@@ -0,0 +1,20 @@
+/* !
+ * @module      : @squirrel-forge/ui-accordion
+ * @version     : 0.5.0
+ * @license     : MIT
+ * @copyright   : 2022 squirrel-forge
+ * @author      : Daniel Hartwell aka. siux <me@siux.info>
+ * @description : An accessible accordion with events and plugin support, made for the browser and babel compatible.
+ */
+
+/**
+ * Accordion
+ */
+export { UiAccordionComponent } from './Accordion/UiAccordionComponent.js';
+export { UiAccordionPanelComponent } from './Accordion/UiAccordionPanelComponent.js';
+
+/**
+ * Plugins
+ */
+export { UiAccordionPluginToggleMode } from './Plugins/UiAccordionPluginToggleMode.js';
+export { UiAccordionPluginScrollFocus } from './Plugins/UiAccordionPluginScrollFocus.js';
