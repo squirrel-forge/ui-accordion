57a7428cf1dde0731648f5edd5c2ecd6874c928f
dasiux <me@siux.info>
2022-05-11 23:14:11 +0200

    scroller and toggle plugin

---
+++ src/es6/Plugins/UiAccordionPluginScroller.js
@@ -0,0 +1,98 @@
+/**
+ * Requires
+ */
+import { UiPlugin } from '../../../../ui-core';
+// import { UiPlugin } from '@squirrel-forge/ui-core';
+
+/**
+ * Ui accordion plugin scroll focus
+ * @class
+ */
+export class UiAccordionPluginScroller extends UiPlugin {
+
+    /**
+     * Plugin name getter
+     * @public
+     * @static
+     * @return {string} - Plugin name
+     */
+    static get pluginName() {
+        return 'scrollfocus';
+    }
+
+    /**
+     * Constructor
+     * @constructor
+     * @param {null|Object} options - Options object
+     * @param {Object|UiAccordionComponent} context - Plugin context
+     * @param {null|console|Object} debug - Debug object
+     */
+    constructor( options, context, debug ) {
+        super( options, context, debug );
+
+        // Extend default config
+        this.extendConfig = {
+
+            // Scroll to panel on events
+            // @type {Array}
+            scrollToOn : [ 'panel.shown' ],
+
+            // Scroll to callback
+            // @type {true|Function}
+            scrollTo : true,
+
+            // Open panel on scroll to panel or content
+            // @type {Array}
+            openOn : [ 'scroll.after' ],
+        };
+
+        // Register events
+        this.registerEvents = [
+            [ 'panel.show', ( event ) => { this.#event_scrollToOn( event ); } ],
+            [ 'panel.shown', ( event ) => { this.#event_scrollToOn( event ); } ],
+            [ 'panel.hide', ( event ) => { this.#event_scrollToOn( event ); } ],
+            [ 'panel.hidden', ( event ) => { this.#event_scrollToOn( event ); } ],
+        ];
+
+        // Register global events
+        window.addEventListener( 'scroll.before', ( event ) => { this.#event_openOn( event ); } );
+        window.addEventListener( 'scroll.after', ( event ) => { this.#event_openOn( event ); } );
+    }
+
+    /**
+     * Event open on
+     * @private
+     * @param {Event} event - Scroll event
+     * @return {void}
+     */
+    #event_openOn( event ) {
+        const events = this.context.config.get( 'openOn' );
+        if ( events.includes( event.type ) ) {
+            this.context.eachChild( ( panel ) => {
+                if ( panel.dom === events.detail.scrollTarget || panel.dom.contains( events.detail.scrollTarget ) ) {
+                    panel.open = true;
+                    return true;
+                }
+            } );
+        }
+    }
+
+    /**
+     * Event scroll to on
+     * @private
+     * @param {Event} event - Accordion event
+     * @return {void}
+     */
+    #event_scrollToOn( event ) {
+        const scrollTo = this.context.config.get( 'scrollTo' );
+        if ( !scrollTo ) return;
+        const events = this.context.config.get( 'scrollToOn' );
+        if ( events.includes( event.type ) ) {
+            if ( typeof scrollTo === 'function' ) {
+                scrollTo( event.detail.target.dom );
+            } else {
+                event.detail.target.dom.scrollIntoView();
+            }
+        }
+    }
+}
---
+++ src/es6/Plugins/UiAccordionPluginToggle.js
@@ -0,0 +1,85 @@
+/**
+ * Requires
+ */
+import { UiPlugin } from '../../../../ui-core';
+// import { UiPlugin } from '@squirrel-forge/ui-core';
+
+/**
+ * Ui accordion plugin toggle mode
+ * @class
+ */
+export class UiAccordionPluginToggle extends UiPlugin {
+
+    /**
+     * Plugin name getter
+     * @public
+     * @static
+     * @return {string} - Plugin name
+     */
+    static get pluginName() {
+        return 'togglemode';
+    }
+
+    /**
+     * Constructor
+     * @constructor
+     * @param {null|Object} options - Options object
+     * @param {Object|UiAccordionComponent} context - Plugin context
+     * @param {null|console|Object} debug - Debug object
+     */
+    constructor( options, context, debug ) {
+        super( options, context, debug );
+
+        // Extend default config
+        this.extendConfig = {
+
+            // Mode
+            // @type {'free'|'toggle'}
+            mode : 'toggle',
+
+            // Close all panels except the first open one on init
+            // @type {boolean}
+            closeAllButFirstOnInit : true,
+        };
+
+        // Register events
+        this.registerEvents = [
+            [ 'children.initialized', ( event ) => { this.#event_childrenInitialized( event ); } ],
+            [ 'panel.show', ( event ) => { this.#event_panelShow( event ); } ],
+        ];
+    }
+
+    /**
+     * Event children.initialized
+     * @private
+     * @return {void}
+     */
+    #event_childrenInitialized() {
+        if ( !this.context.config.get( 'closeAllButFirstOnInit' ) ) return;
+        let has_open = false;
+        this.context.eachChild( ( child ) => {
+
+            // Close every child after we found an open one
+            if ( has_open ) child.open = false;
+
+            // Except the first open one
+            if ( child.open ) has_open = true;
+        } );
+    }
+
+    /**
+     * Event panel.show
+     * @private
+     * @param {Event} event - Panel show event
+     * @return {void}
+     */
+    #event_panelShow( event ) {
+        if ( this.context.mode === 'toggle' && event.detail.target ) {
+            this.context.eachChild( ( child ) => {
+
+                // Close every child that is not the one we are showing
+                if ( child !== event.detail.target ) child.open = false;
+            } );
+        }
+    }
+}
