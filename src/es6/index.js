ff039db83986991b259cfec5ae015b2e8b5aefb5
dasiux <me@siux.info>
2022-05-15 17:43:17 +0200

    update version

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,6 +1,6 @@
 /* !
  * @module      : @squirrel-forge/ui-accordion
- * @version     : 0.6.0
+ * @version     : 0.6.1
  * @license     : MIT
  * @copyright   : 2022 squirrel-forge
  * @author      : Daniel Hartwell aka. siux <me@siux.info>
